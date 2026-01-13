f3dabc01f32fb83c928ef08b869d3cb6267ebd72
yng47 <87718405+yng47@users.noreply.github.com>
2026-01-13 22:00:53 +0400

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,18 @@
+const menuBtn = document.getElementById('menuBtn');
+const popupMenu = document.getElementById('popupMenu');
+
+menuBtn.addEventListener('click', (e) => {
+    popupMenu.classList.toggle('show');
+    // Toggle the active class on the button itself
+    menuBtn.classList.toggle('active');
+    
+    e.stopPropagation();
+});
+
+document.addEventListener('click', (event) => {
+    if (!popupMenu.contains(event.target)) {
+        popupMenu.classList.remove('show');
+        // Remove the active class when clicking outside
+        menuBtn.classList.remove('active');
+    }
+});
